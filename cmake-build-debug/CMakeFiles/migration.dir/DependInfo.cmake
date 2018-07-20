856f843f11784d5ce098d4611ee2eb9472fad672
tyxeron <donxeron@gmail.com>
2018-07-20 19:38:35 +0200

    Makefile changed

---
+++ cmake-build-debug/CMakeFiles/migration.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/libs/lib_migration/trusted/marshalling.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/libs/lib_migration/trusted/marshalling.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/libs/lib_migration/trusted/migration_counters.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/libs/lib_migration/trusted/migration_counters.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/libs/lib_migration/trusted/migration_library.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/libs/lib_migration/trusted/migration_library.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/libs/lib_migration/trusted/migration_library_internal.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/libs/lib_migration/trusted/migration_library_internal.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/libs/lib_migration/trusted/migration_seal.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/libs/lib_migration/trusted/migration_seal.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/migration_enclave/trusted/migration_enclave.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/migration_enclave/trusted/migration_enclave.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/migration_enclave/untrusted/TcpConnection.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/migration_enclave/untrusted/TcpConnection.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/migration_enclave/untrusted/TcpServer.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/migration_enclave/untrusted/TcpServer.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/migration_enclave/untrusted/ra.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/migration_enclave/untrusted/ra.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/migration_enclave/untrusted/sample.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/migration_enclave/untrusted/sample.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/sample_app/src/app/main.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/sample_app/src/app/main.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/sample_app/src/enclave/enclave.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/sample_app/src/enclave/enclave.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/sgx-utils/libs/lib_la/trusted/tla.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/sgx-utils/libs/lib_la/trusted/tla.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/sgx-utils/libs/lib_la/untrusted/ula.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/sgx-utils/libs/lib_la/untrusted/ula.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/sgx-utils/libs/lib_net/src/network_common.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/sgx-utils/libs/lib_net/src/network_common.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/sgx-utils/libs/lib_nrt_ra/nrt_ra.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/sgx-utils/libs/lib_nrt_ra/nrt_ra.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/sgx-utils/libs/lib_tke/tke_nrt.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/sgx-utils/libs/lib_tke/tke_nrt.cpp.o"
+  "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/sgx-utils/libs/lib_uke/uke_nrt.cpp" "/home/joel/Programming/Bachelorthesis/ROTE/ROTE/migration/cmake-build-debug/CMakeFiles/migration.dir/sgx-utils/libs/lib_uke/uke_nrt.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libs/json_cpp"
+  "../libs/lib_migration/trusted"
+  "../libs/libb64"
+  "../libs/libb64/include"
+  "../libs/libb64/include/b64"
+  "../migration_enclave/trusted"
+  "../migration_enclave/untrusted"
+  "../sample_app/src/enclave"
+  "../sgx-utils/libs/lib_la"
+  "../sgx-utils/libs/lib_la/trusted"
+  "../sgx-utils/libs/lib_la/untrusted"
+  "../sgx-utils/libs/lib_net/src"
+  "../sgx-utils/libs/lib_nrt_ra"
+  "../sgx-utils/libs/lib_tke"
+  "../sgx-utils/libs/lib_uke"
+  "/opt/intel/sgxsdk/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/migration.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/migration.dir/libs/lib_migration/trusted/marshalling.cpp.o"
+  "CMakeFiles/migration.dir/libs/lib_migration/trusted/migration_counters.cpp.o"
+  "CMakeFiles/migration.dir/libs/lib_migration/trusted/migration_library.cpp.o"
+  "CMakeFiles/migration.dir/libs/lib_migration/trusted/migration_library_internal.cpp.o"
+  "CMakeFiles/migration.dir/libs/lib_migration/trusted/migration_seal.cpp.o"
+  "CMakeFiles/migration.dir/migration_enclave/trusted/migration_enclave.cpp.o"
+  "CMakeFiles/migration.dir/migration_enclave/untrusted/ra.cpp.o"
+  "CMakeFiles/migration.dir/migration_enclave/untrusted/sample.cpp.o"
+  "CMakeFiles/migration.dir/migration_enclave/untrusted/TcpConnection.cpp.o"
+  "CMakeFiles/migration.dir/migration_enclave/untrusted/TcpServer.cpp.o"
+  "CMakeFiles/migration.dir/sample_app/src/app/main.cpp.o"
+  "CMakeFiles/migration.dir/sample_app/src/enclave/enclave.cpp.o"
+  "CMakeFiles/migration.dir/sgx-utils/libs/lib_la/trusted/tla.cpp.o"
+  "CMakeFiles/migration.dir/sgx-utils/libs/lib_la/untrusted/ula.cpp.o"
+  "CMakeFiles/migration.dir/sgx-utils/libs/lib_net/src/network_common.cpp.o"
+  "CMakeFiles/migration.dir/sgx-utils/libs/lib_nrt_ra/nrt_ra.cpp.o"
+  "CMakeFiles/migration.dir/sgx-utils/libs/lib_tke/tke_nrt.cpp.o"
+  "CMakeFiles/migration.dir/sgx-utils/libs/lib_uke/uke_nrt.cpp.o"
+  "migration.pdb"
+  "migration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/migration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
